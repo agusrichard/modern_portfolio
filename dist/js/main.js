bc5e763cba03ac2c0892263aaa4c0431c5597c3c
Agus Richard <agus.richard21@gmail.com>
2020-01-08 10:46:09 +0000

    Adjusting finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -33,3 +33,27 @@ function toggleMenu() {
 
 	}
 }
+
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.getElementsByClassName('img-projects');
+var modalImg = document.getElementById("imgPop");
+var captionText = document.getElementById("caption");
+for (let i = 0; i < images.length; i++) {
+	images[i].onclick = function(){
+	  modal.style.display = "block";
+	  modalImg.src = this.src;
+	  captionText.innerHTML = this.alt;
+	}
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-img")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
